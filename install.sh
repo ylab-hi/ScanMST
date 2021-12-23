38b0b80519f3b7059f935d77463965456d22bc1f
yangli <li002252@umn.edu>
2021-12-22 22:02:17 -0600

    :fire: remove used files

--- install.sh
+++
@@ -1,6 +0,0 @@
-conda install -c anaconda biopython
-conda install -c conda-forge scikit-bio
-conda install -c bioconda htseq pysam pyfaidx
-git clone https://github.com/brentp/align.git
-
-conda env export -f environment.yml
