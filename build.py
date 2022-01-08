9cb3605dc79c71de0a268873e334491b556227d0
yangli <li002252@umn.edu>
2022-01-07 21:16:15 -0600

    :seedling: add pybind11 and edlib

---
+++ build.py
@@ -0,0 +1,23 @@
+# !/usr/bin/env python
+"""Build cpp extension.
+
+@Filename:    build.py
+@license:     MIT Licence
+@Time:        1/7/22 3:00 PM
+"""
+from pybind11.setup_helpers import build_ext
+from pybind11.setup_helpers import Pybind11Extension
+
+
+def build(setup_kwargs):
+    """Build cpp extension."""
+    ext_modules = [
+        Pybind11Extension("scannls.cppext.align", ["src/scannls/cppext/align.cpp"])
+    ]
+    setup_kwargs.update(
+        {
+            "ext_modules": ext_modules,
+            "cmd_class": {"build_ext": build_ext},
+            "zip_safe": False,
+        }
+    )
