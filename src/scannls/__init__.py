d22accb0b31eb51a47df2a9f5d3de7d78c46ad00
yangli <li002252@umn.edu>
2022-02-04 17:30:44 -0600

    :test_tube: Add test for Read and ReadConnector

--- src/scannls/__init__.py
+++ src/scannls/__init__.py
@@ -17,6 +17,7 @@ from ._class.blat import Blat
 from ._class.myLogger import MyLogger
 from ._class.parallel import ParallelWorker
 from ._class.readConnector import detect_read_read_connections_from_cigar
+from ._class.readConnector import ReadsConnector
 from .utils import get_softclip_length, external_tool_checking
 from ._class.cliqueFinder import CliqueFinder
 from ._class.spliceGraph import SpliceGraph
@@ -38,6 +39,7 @@ __all__ = [
     "Blat",
     "MyLogger",
     "ParallelWorker",
+    "ReadsConnector",
     "detect_read_read_connections_from_cigar",
     "get_softclip_length",
     "SpliceGraph",
