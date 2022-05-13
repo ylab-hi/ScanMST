9372ddd414124d620bbfb21ca01d41524d0eb4ea
Ting-You Wang <dolittle007@gmail.com>
2022-05-12 21:03:45 -0500

    using tuned default params

--- src/scannls/arg.py
+++ src/scannls/arg.py
@@ -42,9 +42,9 @@ class DefaultOptions:
     mismatch: int = 3
     alignment_fraction: float = 0.8
     long_indel_length: int = 5
-    substitutions_num: int = 3
-    substitutions_fraction: float = 0.2
-    indel_fraction: float = 0.2
+    substitutions_num: int = 10
+    substitutions_fraction: float = 0.1
+    indel_fraction: float = 0.1
 
 
 class RichArgParser(argparse.ArgumentParser):
