4aaad543ad482c81308e7a5b9acce4b43f4aabc1
yangli <li002252@umn.edu>
2022-05-16 19:51:13 -0500

    :sparkles: add change_dir decorator

--- src/scannls/utils.py
+++ src/scannls/utils.py
@@ -4,9 +4,11 @@ import os
 import secrets
 import shutil
 import time
+from contextlib import contextmanager
 from functools import wraps
 from typing import Any
 from typing import Callable
+from typing import Generator
 from typing import Tuple
 
 import pysam
@@ -107,3 +109,34 @@ def timeit(func: Callable[..., Any]) -> Callable[..., Any]:
         return result
 
     return wrapped
+
+
+@contextmanager
+def change_dir(path: str) -> Generator:
+    """Change current working directory.
+
+    :param path: the path to be changed
+    """
+    old_dir = os.getcwd()
+    try:
+        os.chdir(path)
+        yield
+    finally:
+        os.chdir(old_dir)
+
+
+def change_dir_decorator(path: str):
+    """Change current working directory.
+
+    :param path: the path to be changed
+    """
+
+    def decorator(func: Callable[..., Any]) -> Callable[..., Any]:
+        @wraps(func)
+        def wrapped(*args, **kwargs):  # type: ignore
+            with change_dir(path):
+                return func(*args, **kwargs)
+
+        return wrapped
+
+    return decorator
