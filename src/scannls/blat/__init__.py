1b4fbe3f756d1ad94d877ccb2f804adc34b53f13
yangli <li002252@umn.edu>
2022-05-23 11:52:06 -0500

    :white_check_mark: add test for blat execuete

--- src/scannls/blat/__init__.py
+++ src/scannls/blat/__init__.py
@@ -1,7 +1,58 @@
-"""Blat excute from ucsc.
+"""Blat executable module.
 
-@author: YangyangLi
-@contact:li002252@umn.edu
-@time: 2022/05/28 10:21 PM
-@source: https://hgdownload.soe.ucsc.edu/admin/exe/macOSX.x86_64/
+@Filename:    __init__.py
+@Author:      YangyangLi
+@contact:     li002252@umn.edu
+@license:     MIT Licence
+@Time:        5/23/22 10:35 AM
+@source: https://hgdownload.soe.ucsc.edu/admin/exe/
 """
+import platform
+from importlib import resources
+from pathlib import Path
+
+PACKAGE_NAME = "scannls_ont"
+
+
+def load_blat() -> Path:
+    """Load blat.
+
+    @return: Path object.
+    """
+    with resources.path(PACKAGE_NAME, "blat") as f:
+        blat_path = f
+    system = platform.system()
+    if system == "Windows":
+        raise NotImplementedError("Windows is not supported for blat.")
+    return blat_path / system.lower()
+
+
+def load_gfserver() -> Path:
+    """Load gfServer.
+
+    @return: Path object.
+    """
+    path = load_blat() / "gfServer"
+    path.chmod(0o755)
+    return path
+
+
+def load_gfclient() -> Path:
+    """Load gfClient.
+
+    @return: Path object.
+    """
+    path = load_blat() / "gfClient"
+    path.chmod(0o755)
+    return path
+
+
+def load_fa2bit():
+    """Load fa2bit.
+
+    Returns:
+        fa2bit: fa2bit object.
+    """
+    path = load_blat() / "faToTwoBit"
+    path.chmod(0o755)
+    return path
---
+++ tests/test_load_blat.py
@@ -0,0 +1,39 @@
+# !/usr/bin/env python
+"""Test for blat executables.
+
+@Filename:    test_load_blat.py
+@Author:      YangyangLi
+@contact:     li002252@umn.edu
+@license:     MIT Licence
+@Time:        5/23/22 11:34 AM
+"""
+import subprocess
+
+from scannls import blat
+
+
+def test_load_blat():
+    """Test for blat path."""
+    blat_path = blat.load_blat()
+    assert blat_path.exists()
+
+
+def test_load_gfserver():
+    """Test for gfserver."""
+    gfserver_path = blat.load_gfserver()
+    assert gfserver_path.exists()
+    subprocess.check_call(gfserver_path)
+
+
+def test_load_gfclient():
+    """Test for gfclient."""
+    gfclient_path = blat.load_gfclient()
+    assert gfclient_path.exists()
+    subprocess.check_call(gfclient_path)
+
+
+def test_load_fa2bit():
+    """Test for fa2bit."""
+    fa2bit_path = blat.load_fa2bit()
+    assert fa2bit_path.exists()
+    subprocess.check_call(fa2bit_path)
