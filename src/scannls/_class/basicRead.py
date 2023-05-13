65bff47e7ba583604a6f4a5f002b92c00b242711
Carlos D. Wang <dolittle007@gmail.com>
2023-05-13 10:02:42 -0500

    Update basicRead.py

--- src/scannls/_class/basicRead.py
+++ src/scannls/_class/basicRead.py
@@ -198,11 +198,11 @@ class Read:
             if op_code in {0, 2}:  # M, D
                 current_pos = current_pos + _len
             elif op_code == 3:  # N
-                exons.append([start_pos, current_pos])
+                exons.append((start_pos, current_pos))
                 current_pos = current_pos + _len
                 start_pos = current_pos
 
-        exons.append([start_pos, current_pos])
+        exons.append((start_pos, current_pos))
 
         introns = []
         # No 'N' in the cigar
@@ -213,7 +213,7 @@ class Read:
             _positions.sort()
             _positions.pop(0)
             _positions.pop(-1)
-            introns = [[x, y] for x, y in zip(_positions[::2], _positions[1::2])]
+            introns = [(x, y) for x, y in zip(_positions[::2], _positions[1::2])]
         return exons, introns
 
     def splice_site_checker(self, genome_fasta, fraction_cutoff=0.6) -> bool:
