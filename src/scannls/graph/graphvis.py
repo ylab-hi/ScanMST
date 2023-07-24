3a2f2f64ebe27d5d53a7b9bf8447d89cbf10d63f
Yangyang Li <yangyang.li@northwestern.edu>
2023-07-24 02:22:23 -0500

    refactor(graphvis): Refactor read_graph and output_graph functions

--- src/scannls/graph/graphvis.py
+++ src/scannls/graph/graphvis.py
@@ -3,24 +3,62 @@
 @Author:      YangyangLi
 @Time:        1/28/22 8:46 PM
 """
+import json
 from pathlib import Path
 
 import networkx as nx
 
 from . import Node
 
+GRAPH_LINK_DATA = {"link": "edges", "source": "from", "target": "to"}
+
+
+def read_graph(file_name: str | Path):
+    if isinstance(file_name, str):
+        file_name = Path(file_name)
+
+    if not file_name.exists():
+        msg = f"{file_name} not exists."
+        raise FileNotFoundError(msg)
+
+    with file_name.open() as f:
+        data = json.load(f)
+        return nx.node_link_graph(data, **GRAPH_LINK_DATA)
+
 
 def get_label_from_node(node: Node) -> str:
     """Get label from node."""
-    return (
-        f"{node.chrom}_{node.ref_start}_{node.ref_end}_{node.sr}_{node.is_start_node()}"
-    )
+    head_node = "S" if node.is_start_node() else "N"
+    return f"{node.chrom}_{node.ref_start}_{node.ref_end}_{node.sr}_{node.strand}_{head_node}"
+
+
+def output_graph(
+    graph,
+    file_name: str | Path,
+):
+    if isinstance(file_name, str):
+        file_name = Path(file_name)
+
+    g = nx.DiGraph()
+    labels = {}
+
+    for start_node in graph.get_start_nodes():
+        if not start_node.successors:
+            g.add_node(get_label_from_node(start_node))
+        else:
+            labels.update({start_node: get_label_from_node(start_node)})
+            plot_graph_helper(start_node, [], g, graph)  # type: ignore
+
+    data = nx.node_link_data(g, **GRAPH_LINK_DATA)
+    with open(f"{file_name}.json", "w", encoding="utf-8") as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
 
 
 def plot_graph_helper(
     start_node: Node,
     path,
     nx_graph: nx.Graph,
+    graph,
     labels: dict[Node, str],
 ) -> None:
     """Plot graph helper."""
@@ -29,34 +67,22 @@ def plot_graph_helper(
 
     if successors := start_node.successors:
         for successor in successors:
-            labels.update({successor: get_label_from_node(successor)})
-            nx_graph.add_edge(
-                get_label_from_node(start_node),
-                get_label_from_node(successor),
-            )
-            plot_graph_helper(successor, [*path, start_node], nx_graph, labels)
+            for edge in graph.get_possible_edges(path, start_node, successor):
+                labels.update({successor: get_label_from_node(successor)})
+                nx_graph.add_edge(
+                    get_label_from_node(start_node),
+                    get_label_from_node(successor),
+                )
+                plot_graph_helper(
+                    successor,
+                    [*path, start_node, edge],
+                    nx_graph,
+                    graph,
+                    labels,
+                )
     else:
         # successor be [] or None
-        plot_graph_helper(
-            successors,
-            [*path, start_node],
-            nx_graph,
-            labels,  # type: ignore
-        )
-
-
-def export_graph(graph, file_name: Path) -> None:
-    """Export graph."""
-    g = nx.DiGraph()
-    labels = {}
-    for start_node in graph.get_start_nodes():
-        if not start_node.successors:
-            g.add_node(get_label_from_node(start_node))
-        else:
-            labels.update({start_node: get_label_from_node(start_node)})
-            plot_graph_helper(start_node, [], g, labels)  # type: ignore
-
-    nx.write_adjlist(g, file_name)
+        plot_graph_helper(successors, [*path, start_node], nx_graph, graph, labels)
 
 
 def plot_graph(graph, figure_name: str, *, is_matplotlib=True) -> None:
