907442e6e0e6f9ac9cc507885fbb0ef5e7226d6f
Yangyang Li <yangyang.li@northwestern.edu>
2023-11-10 14:05:58 -0600

    chore: reorder event in NLPath class

--- src/scannls/graph/basic_graph.py
+++ src/scannls/graph/basic_graph.py
@@ -914,6 +914,7 @@ class NLPath:
         )
 
         if not is_bp1_upstream:
+            logger.trace(f"reorder event {evt=} {is_bp1_upstream=}")
             evt.reverse()
 
         return evt
@@ -999,6 +1000,7 @@ class NLPath:
         blat,
     ) -> NLPath:
         """Create a nlpath from a list of events."""
+
         events = NLPath.order_events_by_trancription_direction(events)
 
         edges_data: list[EdgeData] = []
