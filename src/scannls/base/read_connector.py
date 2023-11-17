2ee1cdda33cd7d4f1770095ab815a85aa76c104c
Tingyou Wang <dolittle007@gmail.com>
2023-11-17 15:51:24 -0600

    auxiliary alignment[1] start/end

--- src/scannls/base/read_connector.py
+++ src/scannls/base/read_connector.py
@@ -573,6 +573,7 @@ class ReadsConnector:
             self.threshold_identity,
             self.mapq_cutoff,
         )
+        self.logger.trace(f"start end read checking: {ret=}")
 
         if ret is None:
             return
@@ -589,10 +590,10 @@ class ReadsConnector:
             )
             # discard blat alignments mapped to uncommon chromosome and mitochondrion
             if "_" not in new_read.chrom and new_read.chrom not in {"chrM", "MT"}:
-                self.logger.trace(
-                    f"auxiliary alignment[1] is effective here. reads_name:{new_read.query_name} query_sequence:{query_sequence}"
-                )
                 if read_type == "start":
+                    self.logger.trace(
+                        f"auxiliary alignment[1](start) is effective here. reads_name:{new_read.query_name} query_sequence:{query_sequence}"
+                    )
                     self.logger.debug(f"Insert {new_read=} to start of reads chain")
                     self.reads_chain.insert(0, new_read)
 
@@ -602,6 +603,9 @@ class ReadsConnector:
                         read.mode,
                     )
                 else:
+                    self.logger.trace(
+                        f"auxiliary alignment[1](end) is effective here. reads_name:{new_read.query_name} query_sequence:{query_sequence}"
+                    )
                     self.logger.debug(f"Add {read=} to reads chain")
                     self.reads_chain.append(new_read)
                     self.logger.debug(f"add {read=}, {new_read=} to mode dict")
