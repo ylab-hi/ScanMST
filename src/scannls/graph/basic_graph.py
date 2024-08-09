e74bfae92e04309c67761dc29ebc85b88f82d2cb
Tingyou Wang <dolittle007@gmail.com>
2024-08-09 16:32:32 -0500

    prefer microhomology when merging edge

--- src/scannls/graph/basic_graph.py
+++ src/scannls/graph/basic_graph.py
@@ -1377,12 +1377,14 @@ def merge_insertion(edge1: Edge, edge2: Edge):
         if edge1.insertion_info is None:
             return
 
+        # prefer blunt end
         if edge2.insertion_info is None:
             edge1.insertion_info = edge2.insertion_info
             return
 
-        if isinstance(edge1.insertion_info[1], MicroHomology) and isinstance(
+        # prefer micorhomology over novelinsertion
+        if isinstance(edge1.insertion_info[1], NovelInsertion) and isinstance(
             edge2.insertion_info[1],
-            NovelInsertion,
+            MicroHomology
         ):
             edge1.insertion_info = edge2.insertion_info
