e59ee832bc92faa410eb338fd578a227360c2374
Tingyou Wang <dolittle007@gmail.com>
2024-11-27 09:41:16 -0600

    increase default edit distance cutoff

--- src/scannls/cli/arg.py
+++ src/scannls/cli/arg.py
@@ -35,7 +35,7 @@ class DefaultOptions:
     species_choices: tuple[str, str] = ("human", "mouse")
     parallel: int = 1
     min_soft_seg_len: int = 200
-    max_allowed_nm: int = 50
+    max_allowed_nm: int = 100
     max_allowed_micro_insertion: int = 50
     ident_cutoff: float = 0.90
     prune_threshold: int = 10  # for merging conditions
