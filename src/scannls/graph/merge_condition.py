db6d385503723aa26610315fecffeeff31e45e5f
Tingyou Wang <dolittle007@gmail.com>
2025-01-10 10:07:53 -0600

    fix: add head2head4distance and tail2tail4distance rules for read clustering

--- src/scannls/graph/merge_condition.py
+++ src/scannls/graph/merge_condition.py
@@ -81,13 +81,20 @@ class MergeCondition:
 
     @staticmethod
     def mid2mid(node1: Node, node2: Node) -> bool:
-        if node1.chrom != node2.chrom or node1.strand != node2.strand or node1.introns != node2.introns:
+        if (
+            node1.chrom != node2.chrom
+            or node1.strand != node2.strand
+            or node1.introns != node2.introns
+        ):
             return False
 
         if node1.exons is None or node2.exons is None:
             raise ValueError
 
-        return node1.exons.first.start == node2.exons.first.start and node1.exons.last.end == node2.exons.last.end
+        return (
+            node1.exons.first.start == node2.exons.first.start
+            and node1.exons.last.end == node2.exons.last.end
+        )
 
     def mid2tail(self, node1: Node, node2: Node) -> bool:
         return _compare_is_merged_helper_check_condition_for_tail_and_middle_nodes_mode(
@@ -104,6 +111,20 @@ class MergeCondition:
 
     def tail2tail(self, node1: Node, node2: Node) -> bool:
         return _compare_is_merged_helper_check_condition_for_two_tail_nodes_mode(
+            node1,
+            node2,
+            self.threshold,
+        )
+
+    def head2head4distance(self, node1: Node, node2: Node) -> bool:
+        return _compare_is_merged_helper_check_condition_for_two_heads_nodes_mode_for_distance(
+            node1,
+            node2,
+            self.threshold,
+        )
+
+    def tail2tail4distance(self, node1: Node, node2: Node) -> bool:
+        return _compare_is_merged_helper_check_condition_for_two_tail_nodes_mode_for_distance(
             node1,
             node2,
             self.threshold,
@@ -136,6 +157,101 @@ class MergeCondition:
             return self.tail2tail(node1, node2)
         msg = "Invalid node identity"
         raise ValueError(msg)
+
+
+def _compare_is_merged_helper_check_condition_for_two_heads_nodes_mode_for_distance(
+    node1: Node,
+    node2: Node,
+    threshold: int,
+    jaccard_threshold: float = 0.5,
+) -> bool:
+    """Check if both head nodes shared most of the introns.
+
+    :param threshold:
+
+    .. note::
+        nodes with different length may be merged. []: exon -: intron
+        node1: [    ]-[ ]-[ ]-[ ]
+        node2: [ ]-[]-[ ]-[ ]-[ ]
+
+        node1: [ ]-----[ ]-[ ]-[ ]
+        node2:     [ ]-[ ]-[ ]-[ ]
+
+    """
+    if node1.chrom != node2.chrom:
+        return False
+
+    # merge will not work for nodes on the different strands
+    if node1.strand != node2.strand:
+        return False
+
+    # checking if breakpoints satisfy the threshold
+    if node1.strand.is_forward():
+        if abs(node1.ref_end - node2.ref_end) > threshold:
+            return False
+
+    elif abs(node1.ref_start - node2.ref_start) > threshold:
+        return False
+
+    # both have no introns
+    if not node1.introns and not node2.introns:
+        return True
+
+    if node1.introns and node2.introns:
+        set1 = set(node1.introns)
+        set2 = set(node2.introns)
+        jaccard_index = len(set1 & set2) / len(set1 | set2)
+        return jaccard_index > jaccard_threshold
+
+    return False
+
+
+def _compare_is_merged_helper_check_condition_for_two_tail_nodes_mode_for_distance(
+    node1: Node,
+    node2: Node,
+    threshold: int,
+    jaccard_threshold: float = 0.5,
+) -> bool:
+    """Check if two end nodes shared most of introns.
+
+    node1 is tail node, node2 is tail node
+    check if they shared at a certain level.
+    alternative polyadenylation (APA) is common phenomenon for genes.
+
+    :param threshold: threshold for checking if two nodes are merged
+    :param node1:  node1
+    :param node2:  node2
+    :return:  True if two nodes are merged, otherwise False
+
+    .. note::
+
+        -> [node1]
+        -> [node2]
+    """
+
+    if node1.chrom != node2.chrom:
+        return False
+
+    # merge will not work for nodes on the different strands
+    if node1.strand != node2.strand:
+        return False
+
+    if node1.strand.is_forward():
+        if abs(node1.ref_start - node2.ref_start) > threshold:
+            return False
+    elif abs(node1.ref_end - node2.ref_end) > threshold:
+        return False
+
+    # both have no introns
+    if not node1.introns and not node2.introns:
+        return True
+
+    if node1.introns and node2.introns:
+        set1 = set(node1.introns)
+        set2 = set(node2.introns)
+        jaccard_index = len(set1 & set2) / len(set1 | set2)
+        return jaccard_index > jaccard_threshold
+    return False
 
 
 def _compare_is_merged_helper_check_condition_for_two_heads_nodes_mode(
@@ -230,9 +346,15 @@ def __intron_lists_containment_checker(node1, node2, reverse=False):
 
     if reverse:
         # For reverse strand, check from start
-        return full_list[:sub_length] == sub_list[:] and full_exons[sub_length].end >= sub_exons.last.end
+        return (
+            full_list[:sub_length] == sub_list[:]
+            and full_exons[sub_length].end >= sub_exons.last.end
+        )
     # For forward strand, check from end
-    return full_list[-sub_length:] == sub_list[:] and full_exons[-(sub_length + 1)].start <= sub_exons.first.start
+    return (
+        full_list[-sub_length:] == sub_list[:]
+        and full_exons[-(sub_length + 1)].start <= sub_exons.first.start
+    )
 
 
 def _compare_is_merged_helper_check_condition_for_head_and_tail_nodes_mode(
@@ -326,7 +448,10 @@ def _compare_is_merged_helper_check_condition_for_two_tail_nodes_mode(
 
     # For nodes with polyA, a small difference in polyA positions is allowed.
     if node1.is_polya and node2.is_polya:
-        return abs(node1.exons.first.start - node2.exons.first.start) <= threshold and abs(node1.exons.last.end - node2.exons.last.end) <= threshold
+        return (
+            abs(node1.exons.first.start - node2.exons.first.start) <= threshold
+            and abs(node1.exons.last.end - node2.exons.last.end) <= threshold
+        )
 
     if node1.is_polya and not node2.is_polya:
         if node1.strand.is_forward():
